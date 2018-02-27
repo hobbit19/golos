7a3253bef994d4bb57f8e862ef870f4d3bd301ef
Vladimir Pavliv <vovchapavliv@gmail.com>
2018-02-27 15:40:21 +0200

    Implemented blocks queue and writing only irreversible blocks to mongo #421

--- libraries/plugins/mongo_db/include/golos/plugins/mongo_db/mongo_db_writer.hpp
+++ libraries/plugins/mongo_db/include/golos/plugins/mongo_db/mongo_db_writer.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <golos/protocol/block.hpp>
+#include <golos/chain/database.hpp>
 
 #include <bsoncxx/builder/basic/kvp.hpp>
 #include <bsoncxx/builder/basic/document.hpp>
@@ -12,6 +13,9 @@
 #include <mongocxx/instance.hpp>
 #include <mongocxx/stdx.hpp>
 #include <mongocxx/uri.hpp>
+
+#include <map>
+
 
 using bsoncxx::builder::stream::close_array;
 using bsoncxx::builder::stream::close_document;
@@ -29,7 +33,7 @@ namespace mongo_db {
 
     class mongo_db_writer final {
     public:
-        mongo_db_writer() = default;
+        mongo_db_writer();
         ~mongo_db_writer() = default;
 
         void initialize(const std::string& uri_str);
@@ -38,15 +42,25 @@ namespace mongo_db {
 
     private:
 
+        void write_blocks();
+        void write_block(const signed_block& block, mongocxx::bulk_write& _bulk);
+
         size_t processed_blocks = 0;
 
         std::string db_name;
-        static const std::string blocks_col;
-        static const std::string trans_col;
+        static const std::string blocks_table;
+        static const std::string trans_table;
 
+        // Key = Block num, Value = block
+        uint32_t last_irreversible_block_num;
+        std::map<uint32_t, signed_block> _blocks;
+
+        // Mongo connection members
         mongocxx::instance mongo_inst;
         mongocxx::uri uri;
         mongocxx::client mongo_conn;
+
+        golos::chain::database &_db;
     };
 }}}
 
--- libraries/plugins/mongo_db/mongo_db_writer.cpp
+++ libraries/plugins/mongo_db/mongo_db_writer.cpp
@@ -1,5 +1,8 @@
 #include <golos/plugins/mongo_db/mongo_db_writer.hpp>
 #include <fc/log/logger.hpp>
+#include <appbase/application.hpp>
+#include <golos/plugins/chain/plugin.hpp>
+
 #include <mongocxx/exception/exception.hpp>
 
 
@@ -11,8 +14,12 @@ namespace mongo_db {
     using namespace bsoncxx::builder;
     using bsoncxx::builder::basic::kvp;
 
-    const std::string mongo_db_writer::blocks_col = "Blocks";
-    const std::string mongo_db_writer::trans_col = "Transactions";
+    const std::string mongo_db_writer::blocks_table = "Blocks";
+    const std::string mongo_db_writer::trans_table = "Transactions";
+
+    mongo_db_writer::mongo_db_writer() :
+            _db(appbase::app().get_plugin<golos::plugins::chain::plugin>().db()) {
+    }
 
     void mongo_db_writer::initialize(const std::string& uri_str) {
 
@@ -23,41 +30,19 @@ namespace mongo_db {
             ilog("MongoDB plugin initialized.");
         }
         catch (mongocxx::exception & ex) {
-            ilog("Exception in MongoDB ctor: ${p}", ("p", ex.what()));
+            ilog("Exception in MongoDB initialize: ${p}", ("p", ex.what()));
         }
     }
 
     void mongo_db_writer::on_block(const signed_block& block) {
         try {
-            auto blocks = mongo_conn[db_name][blocks_col]; // Blocks
 
-            auto doc = document {};
-            doc << "block_num"      << std::to_string(block.block_num())
-                << "block_id"       << block.id().str()
-                << "prev_block_id"  << block.previous.str()
-                << "timestamp"      << block.timestamp
-                << "witness"        << block.witness
-                << "created_at"     << fc::time_point::now();
+            _blocks[block.block_num()] = block;
 
-            if (!block.transactions.empty()) {
-                auto in_array = doc << trans_col << open_array;
-
-                int trx_num = -1;
-                for (const auto& trx : block.transactions) {
-                    ++trx_num;
-
-                    in_array << open_document
-                             << "id"            << trx.id().str()
-                             << "sequence_num"  << std::to_string(trx_num)
-                             << "ref_block_num" << std::to_string(trx.ref_block_num)
-                             << "expiration"    << fc::time_point::now()
-                             << close_document;
-                }
-                in_array << close_array;
-            }
-
-            if (!blocks.insert_one(doc.view())) {
-                ilog("Failed to insert block ${bid}", ("bid", block.id()));
+            last_irreversible_block_num = _db.last_non_undoable_block_num();
+            if (last_irreversible_block_num >= _blocks.begin()->first) {
+                // Having irreversible blocks. Writing em into Mongo.
+                write_blocks();
             }
 
             ++processed_blocks;
@@ -67,4 +52,54 @@ namespace mongo_db {
         }
     }
 
+    void mongo_db_writer::write_blocks() {
+
+        mongocxx::options::bulk_write bulk_opts;
+        bulk_opts.ordered(false);
+        mongocxx::bulk_write _bulk{bulk_opts};
+
+        // Write all the blocks that has num less then last irreversible block
+        while (!_blocks.empty() && _blocks.begin()->first <= last_irreversible_block_num) {
+            auto head_iter = _blocks.begin();
+            write_block(head_iter->second, _bulk);
+            _blocks.erase(head_iter);
+        }
+
+        auto blocks = mongo_conn[db_name][blocks_table]; // Blocks
+
+        if (!blocks.bulk_write(_bulk)) {
+            ilog("Failed to write blocks to Mongo DB");
+        }
+    }
+
+    void mongo_db_writer::write_block(const signed_block& block, mongocxx::bulk_write& _bulk) {
+
+        auto doc = document {};
+        doc << "block_num"      << std::to_string(block.block_num())
+            << "block_id"       << block.id().str()
+            << "prev_block_id"  << block.previous.str()
+            << "timestamp"      << block.timestamp
+            << "witness"        << block.witness
+            << "created_at"     << fc::time_point::now();
+
+        if (!block.transactions.empty()) {
+            auto in_array = doc << "Transactions" << open_array;
+
+            int trx_num = -1;
+            for (const auto& trx : block.transactions) {
+                ++trx_num;
+
+                in_array << open_document
+                         << "id"            << trx.id().str()
+                         << "sequence_num"  << std::to_string(trx_num)
+                         << "ref_block_num" << std::to_string(trx.ref_block_num)
+                         << "expiration"    << fc::time_point::now()
+                         << close_document;
+            }
+            in_array << close_array;
+        }
+
+        mongocxx::model::insert_one insert_msg{doc.view()};
+        _bulk.append(insert_msg);
+    }
 }}}
