53f08ca3aeb81b31494243b73c84e371024490b1
Vladimir Pavliv <vovchapavliv@gmail.com>
2018-03-19 16:20:30 +0200

    Minor bugfix #421

--- plugins/mongo_db/mongo_db_writer.cpp
+++ plugins/mongo_db/mongo_db_writer.cpp
@@ -119,27 +119,17 @@ namespace mongo_db {
         document doc;
         doc << "id"             << tran.id().str()
             << "ref_block_num"  << std::to_string(tran.ref_block_num)
-            << "expiration"     << fc::time_point::now();
+            << "expiration"     << tran.expiration;
 
         array oper_arr;
         if (!tran.operations.empty()) {
             // Write every operation in transaction
             for (const auto& op : tran.operations) {
 
-                try {
-                    operation_writer op_writer;
-                    op.visit(op_writer);
+                operation_writer op_writer;
+                op.visit(op_writer);
 
-                    oper_arr << op_writer.get_document();
-                }
-                catch (std::exception& ex) {
-                    ilog("std::exception in MongoDB op.visit: ${p}", ("p", ex.what()));
-                    throw;
-                }
-                catch (...) {
-                    ilog("Unknown exception in MongoDB op.visit");
-                    throw;
-                }
+                oper_arr << op_writer.get_document();
             }
             doc << operations << oper_arr;
         }
